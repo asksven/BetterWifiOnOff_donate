b66c6a8d2ebf6b537098eb6ec42a0fb85034d729
asksven <sven.knispel@gmail.com>
2013-04-07 23:31:14 +0200

    cleaned up project

--- BetterWifiOnOff_donate/gen/com/example/betterwifionoff_donate/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.betterwifionoff_donate;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = false;
-}
